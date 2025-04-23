ce52560d72d5bb496eb96bb235604b656261c18e
eliezer Maciel <eliezeromaciel@gmail.com>
2025-04-23 08:57:23 -0300

    primeiro exercicio da aula 01 realizado

---
+++ aula01/exercicios.js
@@ -0,0 +1,10 @@
+let capacete = 'usado'
+let mosquetao = 'bem usado'
+
+// arnês
+let cadeirinha = 'usada'
+
+// autoseguro
+let autoSeg = 'top, mas usada'
+
+console.log(`O equipamento CAPACETE está ${capacete}, requerendo PRESENTE DO PROFESSOR`)
