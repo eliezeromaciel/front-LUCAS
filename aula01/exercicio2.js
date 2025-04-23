a8dd2e89a8a3587584c7f33cf191485fcd3a9753
eliezer Maciel <eliezeromaciel@gmail.com>
2025-04-23 15:12:52 -0300

    exercicio 2 da aula 1 realizado com sucesso

---
+++ aula01/exercicio2.js
@@ -0,0 +1,12 @@
+//EXERCÍCIO 2
+
+let secao1 = 5
+let secao2 = 8
+let secao3 = 12
+
+let alturaTotal = secao1 + secao2 + secao3
+
+console.log(`Parabéns. Tu alcançou a altura total de ${alturaTotal}m`)
+
+//nome no Brasil é enfiada, quando inicia e termina uma seçào de via. em inglês é pitch
+console.log(`Infelizmente tu caiu na segunda seção, sua altura máxima foi de ${alturaTotal-secao2}m`)
