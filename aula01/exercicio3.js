5de1ce6af1339bf20a0a5ff759a6c1e1503fe5d3
eliezer Maciel <eliezeromaciel@gmail.com>
2025-04-23 15:32:01 -0300

    exercicio 3 da aula 1 realizado com sucesso

---
+++ aula01/exercicio3.js
@@ -0,0 +1,17 @@
+// exercício 3
+
+let agarrasVia1 = [ 'abaulado', 'monodedo', 'reglete', 'pinça', 'bidedo', 'batente', 'agarrão', 'tridedo' ]
+
+console.log(agarrasVia1)
+console.log(agarrasVia1[0])
+
+//acrescentar uma agarra ao array
+//nome inventado pelo pessoal do Muro, onde treino. é uma agarra horrível de pegar, deixando braço todo torto
+agarrasVia1.push('gaston')
+console.log(agarrasVia1)
+
+
+
+//remover segunda agarra
+agarrasVia1.splice(1,1)
+console.log(agarrasVia1)
