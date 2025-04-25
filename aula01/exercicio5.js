4777cb0d5c5ebad4009ab6808d334f1fe558715a
LucasGFAlves <lucas.g.f.alves@gmail.com>
2025-04-25 08:58:24 -0300

    atualizado exercicio5

--- aula01/exercicio5.js
+++ aula01/exercicio5.js
@@ -30,9 +30,40 @@ let qtdeEntradas = [2, 6, 4, 7, 1, 3, 5]
 
 let entradaLimite = 3
 
+/*
+let cont=0; //início
+while(cont<qtdeEndtradas.length){//condição
+cont++;//incrementador
+}
+*/
+
+/*
+let cont=0;//início
+do{
+    cont++;//incrementador
+}while(cont < qtdeEntradas.length)//condição
+*/
+
+for(let cont =0; cont<qtdeEntradas.length; cont++){
+    if(qtdeEntradas[cont] < entradaLimite ){
+        //console.log("Parabéns você conseguiu em:", qtdeEntradas[cont])
+        //console.log(`Parabéns você conseguiu em: ${qtdeEntradas[cont]}`)
+        console.log("Parabéns você conseguiu em:" + qtdeEntradas[cont])
+    }
+    else if((qtdeEntradas[cont] == entradaLimite )){
+        console.log("Você conseguiu no entrada limite")
+    }
+    else{
+        console.log("Você terminou com:"+qtdeEntradas[cont]+"treine mais vagabundo!")
+    }
+}
+
+
 
 for (item of viasEncadenadas) {
-    item.entradas < entradaLimite
+    
+    qtdeEntradas[i] < Math.ceil(viasEncadenadas[i]/2);
+    
     ? console.log(`TOP, mandou À VISTA a via ${item.nome}, apenas ${item.entradas}`)
     : item.entradas == entradaLimite
     ? console.log(`Muito bem, precisou de apenas ${item.entradas} para mandar a via ${item.nome}`)
@@ -45,7 +76,7 @@ const  performanceEscalador =  () => {
     let viasEncadenadasFiltradas = viasEncadenadas.filter( (elem) => elem.entradas <= entradaLimite    )
     console.log(viasEncadenadasFiltradas)
 }
-// performanceEscalador()
+performanceEscalador
 
 
 
