59d55935bdc13ec36ee54191b7f06f2de5b51620
eliezer Maciel <eliezeromaciel@gmail.com>
2025-05-04 19:17:49 -0300

    exercicios aula 12 modificados e todos realizados

--- aula02/exerciciosaula12.js
+++ aula02/exerciciosaula12.js
@@ -5,9 +5,26 @@ while (i<10) {
   i ++
 }
 
+const exercicioWhile = () => {
+  let i = 0
+  let arrayUsuario = [10, 3, 50, 7, 0]
+  let soma = 0
+  while (arrayUsuario[i] !== 0) {
+    console.log('insira outro número!') 
+    soma = soma + arrayUsuario[i] 
+    i++
+  }
+  console.log('mostrar a soma')
+  console.log(soma)
+}
+exercicioWhile()
 
+
+
+///////////////////////////////////////////////////////////////////
 // EXERCÍCIOS FOR
 // objetivo: imprimir o maior número do array
+//resposta 1
 let arrayDeNumeros = [11, 15, 18, 14, 12, 13]
 let maior = 0
 for ( i = 0 ; i < arrayDeNumeros.length ; i ++){
@@ -17,21 +34,73 @@ for ( i = 0 ; i < arrayDeNumeros.length ; i ++){
   }  
 }
 
+// resposta 2
 const imprimeMaior = ( array ) => {
   let maiorNumero = 0
   for ( i = 0 ; i < array.length ; i ++){
+        
     if (array[i] > maiorNumero)  {
       maiorNumero = array[i]
     }  
   }
   console.log(maiorNumero)
 }
-
 imprimeMaior(arrayDeNumeros)
 
 
 // objetivo: imprimir a soma dos números pares
-let arrayDeNumerosNovo = [11, 15, 18, 14, 12, 13]
+const exercicioFor2 = () => {
+  let array = [11, 15, 18, 14, 12, 13]
+  let soma = 0
+  for ( i = 0; i < array.length; i++ ) {
+    let resto = array[i]% 2
+    resto == 0 ? soma = soma + array[i] :
+      console.log(`número ${array[i]} é ÍMPAR`) 
+  }
+  console.log(soma)
+}
+exercicioFor2()
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// exercícios FOR..OF..
+//objetivo imprimir frase completa
+const exercicioForOf = () => {
+  let arrayPalavras =  ['Oi', 'sumido', 'tudo', 'bem?', 'Saudades']
+  let frase = ''
+  for (let item of arrayPalavras) {
+    frase = (`${frase} ${item}`) 
+  }
+  console.log(`${frase} !!`) 
+}
+exercicioForOf()
+
+//objetivo imprimir soma dos números pares
+const somaParesForOf = () => {
+  let arrayForOff = [11, 15, 18, 14, 12, 13]
+  let somaPares = 0
+  for (let numero of arrayForOff) {
+    let pares = numero % 2
+    !pares ? somaPares = somaPares + numero : console.log(`${numero} é impar`)
+  }
+  console.log(somaPares)
+}
+somaParesForOf()
+
+//objetivo armazenar as palavras mais longas em um novo array.
+const exercicioPalavrasLongas = () => {
+  let arrayPalavrasLongas = [ 'parangaricotirimicoaru', 'blasfêmia', 'veraneando', 'imprevisibilidade', 'aparentemente', 'inconstitucionalicimamente']
+  let palavraParametro = 'barbaridade'
+  let arrayPalavrasMaiores = []
+  for (let item of arrayPalavrasLongas) {
+    item.length > palavraParametro.length ? arrayPalavrasMaiores.push(item) : console.log(`A palavra ${item} é menor do que a palavra padrão`)
+  }
+  console.log(arrayPalavrasLongas)
+}
+exercicioPalavrasLongas()
+
 
 
 
@@ -71,4 +140,4 @@ let arrayDeNumerosNovo = [11, 15, 18, 14, 12, 13]
 // (async () => {
 //   await main();
 //   console.log(numeroinicial);
-// })();
+// })();:
